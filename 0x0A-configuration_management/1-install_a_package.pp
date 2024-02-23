83cbc2b9e549c55485ad7dc682b0592edbcf8874
Salma2486 <saloom_001@hotmail.com>
2024-02-23 06:28:19 -0800

    task1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# erth eth etyh ety h
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
