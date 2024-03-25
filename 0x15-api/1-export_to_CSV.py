db082d3c99a1eaa80e4dfef442487e2da30d92c0
Salma2486 <saloom_001@hotmail.com>
2024-03-25 08:09:05 -0700

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,4 +21,4 @@ if __name__ == "__main__":
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos] explain this
+         ) for t in todos]
