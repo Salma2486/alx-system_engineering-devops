7951573c9aee69fb0ec8ee1942f939f1783b2999
Salma2486 <saloom_001@hotmail.com>
2024-03-25 08:05:11 -0700

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""Python script that, using this REST API, for
+a given employee ID, returns information about
+ his/her TO list progress.
+ and export into csv"""
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
+    id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    user = requests.get(url + "users/{}".format(id)).json()
+    # *get todo where userID = id
+    todos = requests.get(url + "todos", {"userId": id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    name = user.get("username")
+
+    file_name = "{}.csv".format(id)
+    with open(file_name, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+
+        for item in todos:
+            completed_status = item.get("completed")
+            task_title = item.get("title")
+
+            writer.writerow([id, name, completed_status, task_title])
